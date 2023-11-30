ddf25a9bc0daad31ab8c0a63ddcd85b3d2674968
珞林 <leewei126@163.com>
2023-11-30 14:17:03 -0800

    Cleaned pipeline 4

--- pipeline_4_regwrt.sv
+++ pipeline_4_regwrt.sv
@@ -1,30 +1,29 @@
 module pipeline_4_regwrt (
-    control_in,memvalue_in,result_in,clk,rst,
-    writeback_data,writenum,write
-);
-    parameter control_width=22;  
-    input [control_width-1:0] control_in;
-    input [15:0] memvalue_in; //this one need no pipeline reg! reg included in the sync-ram output
-    input [15:0] result_in;
-    input clk,rst;
+    input [21:0] control_in,
+    input [15:0] rdata_in, //this one need no pipeline reg! reg included in the sync-ram output
+    input [15:0] result_in,
 
-    output [15:0] writeback_data;
-    output [2:0] writenum;
-    output write;
+    input clk,rst,
+
+    output [15:0] writeback_data_out,
+    output [2:0] writenum_out,
+    output write_out
+); 
 
     wire [15:0] result;
-    wire [control_width-1:0] control_out;
-    vDFF #16 result_reg (clk,rst,result_in,result);
-    vDFF #22 control (clk,rst,control_in,control_out); // USE OF PARAMETER WILL CAUSE ERRORS!!!
+    wire [21:0] control;
+    vDFF #16 pREG_result (clk,rst,result_in,result);
+    vDFF #22 pREG_control (clk,rst,control_in,control); // USE OF PARAMETER WILL CAUSE ERRORS!!!
 
 
     wire [2:0] opcode;
-    assign opcode=control_out[21:19];
+    assign opcode=control[21:19];
+
     wire is_ldr;
     assign is_ldr=(opcode==3'b011);
-    assign writeback_data=is_ldr?memvalue_in:result;//for LDR, we are writting back memory data
-    assign write=control_out[3];
-    assign writenum=control_out[2:0];
 
-    
+    assign writeback_data_out=is_ldr?rdata_in:result;//for LDR, we are writting back memory data
+    assign write_out=control[3];
+    assign writenum_out=control[2:0];
+
 endmodule
