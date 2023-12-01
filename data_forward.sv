032a86a4667755ddb070fd0e0eb49246dfd9f126
珞林 <leewei126@163.com>
2023-12-01 13:43:43 -0800

    Update data_forward.sv

--- data_forward.sv
+++ data_forward.sv
@@ -26,14 +26,14 @@ module data_forward (
     output reg [15:0] data_forwarded_out
 );
 
-    reg [6:1] has_hazard;/*
+    reg [6:1] has_hazard;
     assign has_hazard[1]=(num_reg_in==num_m1_in)&&m1_write_in;
     assign has_hazard[2]=(num_reg_in==num_m2_in)&&m2_write_in;
     assign has_hazard[3]=(num_reg_in==num_m3_in)&&m3_write_in;
     assign has_hazard[4]=(num_reg_in==num_m4_in)&&m4_write_in;
     assign has_hazard[5]=(num_reg_in==num_m5_in)&&m5_write_in;
-    assign has_hazard[6]=(num_reg_in==num_m6_in)&&m6_write_in;*/
-
+    assign has_hazard[6]=(num_reg_in==num_m6_in)&&m6_write_in;
+/*
     always @(*) begin
         has_hazard[1]=(num_reg_in==num_m1_in)&&m1_write_in;
         has_hazard[2]=(num_reg_in==num_m2_in)&&m2_write_in;
@@ -50,6 +50,24 @@ module data_forward (
             6'b100000: data_forwarded_out=data_m6_in;
             default: data_forwarded_out=num_reg_in;
         endcase
+    end
+    */
+    always @(*) begin
+        if (has_hazard[1]) begin
+            data_forwarded_out=data_m1_in;
+        end else if (has_hazard[2]) begin
+            data_forwarded_out=data_m2_in;
+        end else if (has_hazard[3]) begin
+            data_forwarded_out=data_m3_in;
+        end else if (has_hazard[4]) begin
+            data_forwarded_out=data_m4_in;
+        end else if (has_hazard[5]) begin
+            data_forwarded_out=data_m5_in;
+        end else if (has_hazard[6]) begin
+            data_forwarded_out=data_m6_in;
+        end else begin
+            data_forwarded_out=num_reg_in;
+        end
     end
     
 endmodule
