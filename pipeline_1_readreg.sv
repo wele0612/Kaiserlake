9a8f7836f2c4c08b2a9ca170c80c95c653b198ee
珞林 <leewei126@163.com>
2023-12-01 17:49:03 -0800

    Add pipeline regs for used_RmRnRd

--- pipeline_1_readreg.sv
+++ pipeline_1_readreg.sv
@@ -4,6 +4,7 @@ module pipeline_1_readreg (
     input [2:0] num_Rn_in,
     input [2:0] num_Rd_in,
     input [15:0] imm_in,
+    input [2:0] used_RmRnRd_in,
 
     input rst,
     input clk,
@@ -14,14 +15,16 @@ module pipeline_1_readreg (
     output wire [2:0] num_Rn_out,
     output wire [2:0] num_Rd_out,
     output wire [15:0] imm_out,
+    output wire [2:0] used_RmRnRd_out,
 
     output wire loads
 );
-    vDFF_en #22 pREG_control (clk,rst,update,control_in,control_out); //instantiate "control",    USE OF PARAMETER WILL CAUSE ERRORS!!!
+    vDFF_en #22 pREG_control (clk,rst,update,control_in,control_out); 
     vDFF_en #3 pREG_num_Rm (clk,rst,update,num_Rm_in,num_Rm_out); //instantiate "reg nums_a"
     vDFF_en #3 pREG_num_Rn (clk,rst,update,num_Rn_in,num_Rn_out); //instantiate "reg nums_b"
-    vDFF_en #3 pREG_num_Rd (clk,rst,update,num_Rd_in,num_Rd_out);
+    vDFF_en #3 pREG_num_Rd (clk,rst,update,num_Rd_in,num_Rd_out); //注释没活儿可以不写
     vDFF_en #16 pREG_imm (clk,rst,update,imm_in,imm_out); //instantiate "imm"
+    vDFF_en #3 pREG_used_RmRnRd (clk,rst,update,used_RmRnRd_in,used_RmRnRd_out)
 
     assign loads=control_out[8];
 
