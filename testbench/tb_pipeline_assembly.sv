e510ce4f5aa987e944003f33f22b8b97a54671c2
EOW319 <040319yiou@gmail.com>
2023-12-01 20:12:39 -0800

    tb

--- testbench/tb_pipeline_assembly.sv
+++ testbench/tb_pipeline_assembly.sv
@@ -121,13 +121,27 @@ module tb_pipeline_forward ();
         @(posedge clk);
         //ADD R3,R1,R0,LSL#1
         //ADD R4,R1,R0,LSL#1
-        p0_IR_in=16'b101_00_001_011_01_000;
-        p1_IR_in=16'b101_00_001_100_01_000;
+        p0_IR_in=16'B1010000101101000;
+        p1_IR_in=16'B1010000110010000;
         @(posedge clk);
         //ADD R2,R1,R3
-        //AND R5,R2,R4,LSL#1
+        //AND R5,R2,R4
         p0_IR_in=16'b101_00_001_010_00_011;
-        p1_IR_in=16'b101_10_010_101_01_100;
+        p1_IR_in=16'b1011001110100100;
+        @(posedge clk);
+        p0_IR_in=16'b1100000010001001;  //MOV R4,R1,LSL#1 
+        p1_IR_in=16'b1100000010010001;  //MOV R4,R1,LSR#1 
+        @(posedge clk);
+        p0_IR_in=16'b1010110000000000;  //CMP R4,R0 
+        p1_IR_in=16'b1011100010100100;  //MVN R5,R4
+        @(posedge clk);
+        p0_IR_in=16'b1010110100000000;  //CMP R5,R0 
+        //p0_IR_in=16'd0;
+        p1_IR_in=16'b1010100100000100;  //CMP R1,R4
+        @(posedge clk);
+        p0_IR_in=16'b0110000110100000;  //LDR R5,[R1]
+        p1_IR_in=16'b1000000101000000;  //STR R1,[R1]
+        force CPU.p0_DM_rdata=16'd100;
         @(posedge clk);
         p0_IR_in=16'b0;
         p1_IR_in=16'b0;
@@ -136,7 +150,9 @@ module tb_pipeline_forward ();
         @(posedge clk);
         @(posedge clk);
         @(posedge clk);
-        @(posedge clk);
+
+        #10;
+        $stop;
 
     end
 endmodule
