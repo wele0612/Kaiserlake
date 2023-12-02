6138be30611d6bae6d7d57f2beffd22dd0b5eed3
珞林 <leewei126@163.com>
2023-12-02 00:33:09 -0800

    Create vDFF_nr.sv

---
+++ vDFF_nr.sv
@@ -0,0 +1,12 @@
+module vDFF_nr(clk,D,Q);
+  parameter n=1;
+  input clk;
+  input [n-1:0] D;
+  output [n-1:0] Q;
+  reg [n-1:0] Q;
+
+  always @(posedge clk)begin
+    Q <= D;
+  end
+    
+endmodule
