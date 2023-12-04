d488fbcc024ffcd0edfcb7b8da93aef143a08ff9
珞林 <leewei126@163.com>
2023-12-03 20:54:12 -0800

    Minimum BGU to autofetch

--- BGU.sv
+++ BGU.sv
@@ -1,5 +1,13 @@
 module BGU (
-    
+    input [7:0] PC,
+    input fetch_next_in,
+
+    output [7:0] PC_next
 );
+
+    wire [7:0] PC_acc_2;
+    assign PC_acc_2={{PC[7:1]+1},PC[0]};
+
+    assign PC_next=fetch_next_in?PC_acc_2:PC;
     
 endmodule
